47879de90f7700d9163aa21fee2a0e7bfa6884d7
Kequn Yi <kequnyi@Kequns-MacBook-Air.local>
2022-10-08 13:10:03 -0400

    lab 2

---
+++ src/model/Profile.java
@@ -0,0 +1,118 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import javax.swing.Icon;
+
+/**
+ *
+ * @author kequnyi
+ */
+public class Profile {
+    private String name;
+    private int ID;
+    private int age;
+    private String gender;
+    private String startDate;
+    private int level;
+    private String teamInfo;
+    private String position;
+    private int cellNum;
+    private String emailAdd;
+    private Icon image;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public String getTeamInfo() {
+        return teamInfo;
+    }
+
+    public void setTeamInfo(String teamInfo) {
+        this.teamInfo = teamInfo;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public int getCellNum() {
+        return cellNum;
+    }
+
+    public void setCellNum(int cellNum) {
+        this.cellNum = cellNum;
+    }
+
+    public String getEmailAdd() {
+        return emailAdd;
+    }
+
+    public void setEmailAdd(String emailAdd) {
+        this.emailAdd = emailAdd;
+    }
+
+    public Icon getImage() {
+        return image;
+    }
+
+    public void setImage(Icon image) {
+        this.image = image;
+    }
+    
+    @Override
+    public String toString() {
+        return name;
+    }
+}
---
+++ src/model/ProfileHistory.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author kequnyi
+ */
+public class ProfileHistory {
+    
+    private ArrayList<Profile> history;
+    
+    public ProfileHistory() {
+        this.history = new ArrayList<Profile>();
+    }
+
+    public ArrayList<Profile> getHistory() {
+        return history;
+    }
+
+    public void setHistory(ArrayList<Profile> history) {
+        this.history = history;
+    }
+    
+    public Profile addNewProfile() {
+        Profile newProfile = new Profile();
+        history.add(newProfile);
+        return newProfile;
+    }
+    
+    public void deleteProfile(Profile pro) {
+        history.remove(pro);
+    }
+    
+}
